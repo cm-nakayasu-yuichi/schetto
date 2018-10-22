7c47a1c040bcb3364fff71197df11fa39d7a71bb
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-10-23 08:51:37 +0900

    UIImage拡張(途中)

--- schetto/Sources/Extensions/UIImage+App.swift
+++ schetto/Sources/Extensions/UIImage+App.swift
@@ -2,8 +2,53 @@
 //  MyProject
 //  Copyright (c) Yuichi Nakayasu. All rights reserved.
 //
-import Foundation
+import UIKit
 
-extension Date {
+extension UIImage {
     
+    /// 画像ファイルパスから画像を初期化する
+    /// - parameter path: 画像ファイルパス
+    convenience init?(path: String) {
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: path)) else {
+            return nil
+        }
+        self.init(data: data)
+    }
+    
+
+}
+
+extension UIImage {
+    
+    /// PNG形式のデータ
+    var pngData: Data? {
+        return UIImagePNGRepresentation(self)
+    }
+    
+    /// JPEG形式のデータ
+    var jpegData: Data? {
+        return jpegData(compressionQuality: 0.96)
+    }
+    
+    /// JPEG形式のデータ
+    /// - Parameter compressionQuality: 圧縮品質
+    /// - Returns: JPEG形式のデータ
+    func jpegData(compressionQuality: CGFloat) -> Data? {
+        return UIImageJPEGRepresentation(self, compressionQuality)
+    }
+    
+    /// 指定したファイルパスに画像(PNG形式)を保存する
+    /// - Parameter path: ファイルパス
+    func write(to path: String) {
+        try? pngData?.write(to: URL(fileURLWithPath: path), options: [.atomic])
+    }
+    
+    
+    /// 指定したファイルパスに画像(JPEG形式)を保存する
+    /// - Parameters:
+    ///   - path: ファイルパス
+    ///   - compressionQuality: 圧縮品質
+    func writeJpeg(to path: String, compressionQuality: CGFloat) {
+        try? jpegData(compressionQuality: compressionQuality)?.write(to: URL(fileURLWithPath: path), options: [.atomic])
+    }
 }
