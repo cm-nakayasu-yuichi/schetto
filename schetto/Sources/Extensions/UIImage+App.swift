faad96d7ef29055771ce26526dcace16c6fd7681
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-10-23 10:28:33 +0900

    UIImage拡張

--- schetto/Sources/Extensions/UIImage+App.swift
+++ schetto/Sources/Extensions/UIImage+App.swift
@@ -52,22 +52,6 @@ extension UIImage {
 
 extension UIImage {
     
-    /// グラフィックコンテキストの描画処理をラッピングして画像を生成する
-    /// - Parameters:
-    ///   - size: サイズ
-    ///   - processBlock: 描画処理
-    /// - Returns: 生成した画像
-    class func imageFromContext(_ size: CGSize, _ processBlock: (CGContext)->Void) -> UIImage {
-        UIGraphicsBeginImageContext(size)
-        processBlock(UIGraphicsGetCurrentContext()!)
-        let ret = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return ret!
-    }
-}
-
-extension UIImage {
-    
     /// 指定のサイズに拡大縮小した画像を生成して返す
     /// - Parameter size: サイズ
     /// - Returns: 拡大縮小した画像
@@ -88,17 +72,46 @@ extension UIImage {
     }
     
     /// 画像のサイズは固定したまま指定のサイズへと変更した画像を生成して返す
-    /// - Parameter size: 指定のサイズ
+    ///
+    /// - Parameters:
+    ///   - size: 指定のサイズ
+    ///   - position: 位置(省略した(nilの)場合は中央位置)
     /// - Returns: 指定のサイズへと変更した画像
-    func fixed(size: CGSize) -> UIImage {
-        return UIImage.imageFromContext(size) { _ in
-            let rect = CGRect(
-                (size.width - self.size.width) / 2,
-                (size.height - self.size.height) / 2,
-                self.size.width,
-                self.size.height
+    func resized(to size: CGSize, position: CGPoint? = nil) -> UIImage {
+        let pos: CGPoint
+        if position != nil {
+            pos = position!
+        } else {
+            pos = CGPoint(
+                x: (self.size.width - size.width) / 2,
+                y: (self.size.height - size.height) / 2
             )
+        }
+        return UIImage.imageFromContext(size) { _ in
+            let rect = CGRect(origin: pos, size: self.size)
             draw(in: rect)
+        }
+    }
+    
+    /// 指定した画像を重ね合わせた画像を生成して返す
+    /// - Parameters:
+    ///   - image: 重ねる画像
+    ///   - position: 位置
+    /// - Returns: 合成した画像
+    func synthesized(image: UIImage, position: CGPoint? = nil) -> UIImage {
+        return UIImage.imageFromContext(size) { _ in
+            self.draw(in: CGRect(origin: .zero, size: size))
+            
+            let pos: CGPoint
+            if position != nil {
+                pos = position!
+            } else {
+                pos = CGPoint(
+                    x: (size.width - image.size.width) / 2,
+                    y: (size.height - image.size.height) / 2
+                )
+            }
+            image.draw(in: CGRect(origin: pos, size: image.size))
         }
     }
     
@@ -131,9 +144,75 @@ extension UIImage {
         context.draw(imageRef, in: rect)
         return UIImage(cgImage: context.makeImage()!)
     }
+    
+    /// 指定したサイズに収まるように拡大縮小(またはリサイズ)と切り取りをを行った画像を生成して返す
+    /// - Parameters:
+    ///   - size: 変更後のサイズ
+    ///   - expand: 拡大縮小させる(元画像を引き延ばす)かどうか
+    /// - Returns: 調整されたがぞ合う
+    func adjusted(to size: CGSize, shouldExpand expand: Bool = true) -> UIImage {
+        
+        // 再帰的に適切なサイズを探す内部関数
+        func findProperSize(_ src: CGSize, dst: CGSize, reverse: Bool = false) -> CGSize {
+            
+            let scale: CGFloat, w: CGFloat, h: CGFloat
+            
+            if (!reverse && src.width > src.height) || (reverse && src.height > src.width) {
+                scale = dst.width / src.width
+                w = dst.width
+                h = CGFloat(lroundf(Float(src.height * scale)))
+            } else {
+                scale = dst.height / src.height
+                w = CGFloat(lroundf(Float(src.width * scale)))
+                h = dst.height
+            }
+            
+            let ret = CGSize(width: w, height: h)
+            if w < dst.width || h < dst.height {
+                return findProperSize(ret, dst: dst, reverse: !reverse)
+            } else {
+                return ret
+            }
+        }
+        let scaledSize = findProperSize(self.size, dst: size)
+        
+        // 拡大縮小されたサイズから切り取る領域を計算
+        var cropRect = CGRect(origin: .zero, size: size)
+        if scaledSize.width > size.width {
+            cropRect.origin.x = (scaledSize.width - size.width) / 2
+        }
+        if scaledSize.height > size.height {
+            cropRect.origin.y = (scaledSize.height - size.height) / 2
+        }
+        
+        // 拡大縮小実行
+        var ret: UIImage
+        if expand {
+            ret = self.scaled(to: scaledSize)
+        } else {
+            ret = self.resized(to: scaledSize)
+        }
+        // 切り取り実行
+        ret = ret.cropped(to: cropRect)
+        
+        return ret
+    }
 }
 
 extension UIImage {
+    
+    /// グラフィックコンテキストの描画処理をラッピングして画像を生成する
+    /// - Parameters:
+    ///   - size: サイズ
+    ///   - processBlock: 描画処理
+    /// - Returns: 生成した画像
+    class func imageFromContext(_ size: CGSize, _ processBlock: (CGContext) -> Void) -> UIImage {
+        UIGraphicsBeginImageContext(size)
+        processBlock(UIGraphicsGetCurrentContext()!)
+        let ret = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return ret!
+    }
     
     /// 文字を画像化して返す
     /// - Parameters:
@@ -150,6 +229,92 @@ extension UIImage {
         let size = ns.size(withAttributes: attributes)
         return UIImage.imageFromContext(size) { _ in
             ns.draw(at: .zero, withAttributes: attributes)
+        }
+    }
+    
+    /// 空の画像を返す
+    /// - Parameter size: サイズ
+    /// - Returns: 空の画像
+    class func emptyImage(size: CGSize) -> UIImage {
+        return imageFromContext(size) { _ in }
+    }
+    
+    /// 指定した色で埋めた画像を生成して返す
+    /// - Parameters:
+    ///   - color: 色
+    ///   - size: サイズ
+    /// - Returns: 指定した色で埋めた画像
+    class func filled(color: UIColor?, size: CGSize) -> UIImage {
+        return imageFromContext(size) { context in
+            let filledColor = color ?? UIColor.clear
+            context.saveGState()
+            context.setFillColor(filledColor.cgColor)
+            context.fill(CGRect(origin: CGPoint.zero, size: size))
+            context.restoreGState()
+        }
+    }
+    
+    /// 透明のような見た目の画像を指定したサイズで生成して返す
+    /// - Parameters:
+    ///   - size: サイズ
+    ///   - squareWidth: マス目の一辺の幅
+    /// - Returns: 透明のような見た目の画像
+    class func transparentImage(size: CGSize, squareWidth: CGFloat = 10.0) -> UIImage {
+        return imageFromContext(size) { context in
+            let square = squareWidth
+            var oddRow = false
+            var oddCol = false
+            
+            let remainerWidth = size.width.truncatingRemainder(dividingBy: squareWidth)
+            let remainerHeight = size.height.truncatingRemainder(dividingBy: squareWidth)
+            var loc = CGPoint(x: -(remainerWidth / 2), y: -(remainerHeight / 2))
+            
+            while loc.y < size.height {
+                oddRow = !oddRow
+                oddCol = oddRow
+                while loc.x < size.width {
+                    let color = oddCol ? UIColor.white.withAlphaComponent(0.6) : UIColor.white.withAlphaComponent(0.3)
+                    let frame = CGRect(x: loc.x, y: loc.y, width: square, height: square)
+                    context.saveGState()
+                    context.setFillColor(color.cgColor)
+                    context.fill(frame)
+                    context.restoreGState()
+                    loc.x += square
+                    oddCol = !oddCol
+                }
+                loc.x = -(size.width.truncatingRemainder(dividingBy: squareWidth) / 2)
+                loc.y += square
+            }
+        }
+    }
+}
+
+extension UIImage {
+    
+    var retina: UIImage {
+        return UIImage(cgImage: self.cgImage!, scale: UIScreen.main.scale, orientation: .up)
+    }
+}
+
+extension Array where Element == UIImage {
+    
+    /// すべての画像を順に合成した画像を作成する
+    /// - Parameter center: true 画像は中央に配置される。false 画像は左上に配置される
+    /// - Returns: 合成した画像
+    func synthesize(center: Bool = true) -> UIImage? {
+        guard let size = first?.size else { return nil }
+        return UIImage.imageFromContext(size) { _ in
+            forEach {
+                if center {
+                    let pos = CGPoint(
+                        x: (size.width - $0.size.width) / 2,
+                        y: (size.height - $0.size.height) / 2
+                    )
+                    $0.draw(in: CGRect(origin: pos, size: $0.size))
+                } else {
+                    $0.draw(in: CGRect(origin: .zero, size: $0.size))
+                }
+            }
         }
     }
 }
