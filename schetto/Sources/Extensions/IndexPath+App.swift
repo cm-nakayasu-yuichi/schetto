51c58b22ef8a4b647076bdddeecc6c160268eef1
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-10-25 00:11:48 +0900

    IndexPath 拡張

---
+++ schetto/Sources/Extensions/IndexPath+App.swift
@@ -0,0 +1,12 @@
+//
+//  MyProject
+//  Copyright (c) Yuichi Nakayasu. All rights reserved.
+//
+import Foundation
+
+extension IndexPath {
+    
+   init(_ row: Int, section: Int = 0) {
+        self.init(row: row, section: section)
+    }
+}
