42d6d621e7b7f8d025f816b9f0faae2f970aa339
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-10-24 14:07:57 +0900

    アプリケーションのライフサイクル管理

--- schetto/Sources/Applications/AppDelegate.swift
+++ schetto/Sources/Applications/AppDelegate.swift
@@ -8,8 +8,36 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
+    var lifeCycle: ApplicationLifeCycle = .none
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        lifeCycle = .willResignActive
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        lifeCycle = .didEnterBackground
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        lifeCycle = .willEnterForeground
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        lifeCycle = .didBecomeActive
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        lifeCycle = .willTerminate
+    }
+}
+
+extension UIApplication {
+    
+    var lifeCycle: ApplicationLifeCycle {
+        return (UIApplication.shared.delegate as? AppDelegate)?.lifeCycle ?? .none
     }
 }
---
+++ schetto/Sources/Utils/ApplicationLifeCycle.swift
@@ -0,0 +1,14 @@
+//
+//  MyProject
+//  Copyright (c) Yuichi Nakayasu. All rights reserved.
+//
+import UIKit
+
+enum ApplicationLifeCycle {
+    case none
+    case willResignActive
+    case didEnterBackground
+    case willEnterForeground
+    case didBecomeActive
+    case willTerminate
+}
