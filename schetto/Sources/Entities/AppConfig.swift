67e68c982903dd242e6a51e2f83fbee70c01b4a2
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-10-25 00:20:45 +0900

    Configurable追加

---
+++ schetto/Sources/Entities/AppConfig.swift
@@ -0,0 +1,10 @@
+//
+//  MyProject
+//  Copyright (c) Yuichi Nakayasu. All rights reserved.
+//
+import Foundation
+
+class AppConfig: Configurable {
+    
+    dynamic var isFirstLaunch: Bool = false
+}
