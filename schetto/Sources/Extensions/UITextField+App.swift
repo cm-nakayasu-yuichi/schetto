34209682e84f572d3964511b122c27319a6b5a37
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-10-25 18:19:45 +0900

    テキストフィールドの拡張

---
+++ schetto/Sources/Extensions/UITextField+App.swift
@@ -0,0 +1,35 @@
+//
+//  MyProject
+//  Copyright (c) Yuichi Nakayasu. All rights reserved.
+//
+import UIKit
+
+extension UITextField {
+    
+    @IBInspectable var placeholderColor: UIColor? {
+        get {
+            var range = NSMakeRange(0, 1)
+            guard
+                let placeholder = attributedPlaceholder,
+                let ret = placeholder.attribute(.foregroundColor, at: 0, effectiveRange: &range) as? UIColor
+                else {
+                    return nil
+            }
+            return ret
+        }
+        set {
+            guard
+                let color = newValue,
+                let font = font,
+                let text = placeholder
+                else {
+                    return
+            }
+            attributedPlaceholder = NSAttributedString(string: text, attributes: [
+                .foregroundColor : color,
+                .font : font,
+                ]
+            )
+        }
+    }
+}
