c91136914128206b5d8ecd4b2e7e30d2ce9d8486
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-10-25 18:19:58 +0900

    テストUI整備

--- schetto/Sources/Views/Test/TestViewController.swift
+++ schetto/Sources/Views/Test/TestViewController.swift
@@ -8,11 +8,12 @@ class TestViewController: UIViewController {
     
     @IBOutlet private weak var closeButton: UIButton!
     
-    @IBOutlet private weak var child: UIView!
+    @IBOutlet private weak var textField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        _ = child.addConstraint(allSideSpaceTo: nil)
+        _ = textField.addConstraint(top: 16, trailing: -16, bottom: nil, leading: 16, to: self.view.safeAreaLayoutGuide)
+        _ = textField.addConstraint(height: 40)
     }
     
     @IBAction private func didTapCloseButton() {
