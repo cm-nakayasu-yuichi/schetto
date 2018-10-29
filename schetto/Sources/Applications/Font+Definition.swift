a617460d104b848a9a4f1526f8629f80b849a73d
中安佑一 <yuichi.nakayasu@gmail.com>
2018-10-30 02:10:36 +0900

    フォントの定義

---
+++ schetto/Sources/Applications/Font+Definition.swift
@@ -0,0 +1,19 @@
+//
+//  MyProject
+//  Copyright (c) Yuichi Nakayasu. All rights reserved.
+//
+import UIKit
+
+extension UIFont {
+    
+    static let subhead = applicationFont(size: 16)
+    static let subheadBold = applicationBoldFont(size: 16)
+    
+    static func applicationFont(size: CGFloat) -> UIFont {
+        return UIFont(name: "ArialMT", size: size)!
+    }
+    
+    static func applicationBoldFont(size: CGFloat) -> UIFont {
+        return UIFont(name: "Arial-BoldMT", size: size)!
+    }
+}
