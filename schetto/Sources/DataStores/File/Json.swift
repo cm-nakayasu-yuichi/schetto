01361c33bdca46bbe9a713fb29a29af152270582
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-10-31 11:20:13 +0900

    JSONを扱うクラス追加

---
+++ schetto/Sources/DataStores/File/Json.swift
@@ -0,0 +1,73 @@
+//
+//  MyProject
+//  Copyright (c) Yuichi Nakayasu. All rights reserved.
+//
+import Foundation
+
+class Json {}
+
+extension Json {
+    
+    func encode<T>(_ object: T) -> String where T : Encodable {
+        let data = encodeData(object)
+        return String(data: data, encoding: .utf8) ?? ""
+    }
+    
+    func encodeData<T>(_ object: T) -> Data where T : Encodable {
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        encoder.outputFormatting = .prettyPrinted
+        return (try? encoder.encode(object)) ?? Data()
+    }
+}
+
+extension Json {
+    
+    func decode<T>(path: String, to type: T.Type) -> T? where T : Decodable {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        guard
+            let data = try? Data(contentsOf: URL(fileURLWithPath: path)),
+            let ret = try? decoder.decode(type, from: data)
+            else {
+                return nil
+        }
+        return ret
+    }
+    
+    func decode<T>(path: String, to type: T.Type, substitute: T) -> T where T : Decodable {
+        return decode(path: path, to: type) ?? substitute
+    }
+}
+
+extension Json {
+    
+    func serialize<T>(_ object: T) -> String where T : Encodable {
+        let data = serializeData(object)
+        return String(data: data, encoding: .utf8) ?? ""
+    }
+    
+    func serializeData<T>(_ object: T) -> Data where T : Encodable {
+        let options: JSONSerialization.WritingOptions = .prettyPrinted
+        return (try? JSONSerialization.data(withJSONObject: object, options: options)) ?? Data()
+    }
+}
+
+extension Json {
+    
+    func deserialize<T>(path: String, to type: T.Type) -> T? where T : Decodable {
+        let options: JSONSerialization.ReadingOptions = .allowFragments
+        guard
+            let data = try? Data(contentsOf: URL(fileURLWithPath: path)),
+            let object = try? JSONSerialization.jsonObject(with: data, options: options),
+            let ret = object as? T
+            else {
+                return nil
+        }
+        return ret
+    }
+    
+    func deserialize<T>(path: String, to type: T.Type, substitute: T) -> T where T : Decodable {
+        return deserialize(path: path, to: type) ?? substitute
+    }
+}
