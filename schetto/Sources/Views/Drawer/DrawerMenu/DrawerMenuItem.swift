44928b051839882f30323be1f74ae66569ee63a6
中安佑一 <yuichi.nakayasu@gmail.com>
2018-11-01 01:19:18 +0900

    アイテム調整

--- schetto/Sources/Views/Drawer/DrawerMenu/DrawerMenuItem.swift
+++ schetto/Sources/Views/Drawer/DrawerMenu/DrawerMenuItem.swift
@@ -6,6 +6,7 @@ import UIKit
 
 enum DrawerMenuItemSection {
     case calendars
+    case news
     case mode
     case tool
     case setting
@@ -14,6 +15,7 @@ enum DrawerMenuItemSection {
     var title: String {
         switch self {
         case .calendars: return "カレンダー"
+        case .news: return "お知らせ"
         case .mode: return "表示"
         case .tool: return "ツール"
         case .setting: return "設定"
@@ -24,14 +26,16 @@ enum DrawerMenuItemSection {
     var items: [DrawerMenuItem] {
         switch self {
         case .calendars: return [.manageCalendar] // variability.
+        case .news: return [.news]
         case .mode: return [.yearly, .monthly, .weekly, .daily]
         case .tool: return [.search, .dialy]
         case .setting: return [.calendarSetting, .appSetting]
-        case .other: return [.purchase, .help, .acknowledge]
+        case .other: return [.purchase, .help, .terms, .privacy]
         }
     }
     
     static let sections: [DrawerMenuItemSection] = [
+        .news,
         .calendars,
         .mode,
         .tool,
@@ -42,6 +46,7 @@ enum DrawerMenuItemSection {
 
 enum DrawerMenuItem {
     case manageCalendar
+    case news
     case yearly
     case monthly
     case weekly
@@ -52,11 +57,13 @@ enum DrawerMenuItem {
     case appSetting
     case purchase
     case help
-    case acknowledge
+    case terms
+    case privacy
     
     var title: String {
         switch self {
         case .manageCalendar: return "カレンダー管理"
+        case .news: return "お知らせ"
         case .yearly: return "年表示"
         case .monthly: return "月表示"
         case .weekly: return "週表示"
@@ -67,7 +74,8 @@ enum DrawerMenuItem {
         case .appSetting: return "アプリ設定"
         case .purchase: return "アイテム購入"
         case .help: return "ヘルプ"
-        case .acknowledge: return "謝辞"
+        case .terms: return "利用規約"
+        case .privacy: return "プライバシーポリシー"
         }
     }
 }
