09cdcc8d64b7bdb28124a4d038c982df6a1f6846
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-11-02 10:15:38 +0900

    DBオブジェクト一部定義

--- schetto/Sources/Entities/DB/DBEvent.swift
+++ schetto/Sources/Entities/DB/DBEvent.swift
@@ -13,7 +13,8 @@ class DBEvent: RealmSwift.Object, RealmIdentifiedObject {
     @objc dynamic var all = false // 終日フラグ
     @objc dynamic var place = ""
     @objc dynamic var notify = 0 // notify分前に通知する
-    @objc dynamic var color = ""
+    @objc dynamic var color: DBColor?
+    @objc dynamic var sticker: DBSticker?
     
     let linkingCalendar = LinkingObjects(fromType: DBCalendar.self, property: "events")
     
