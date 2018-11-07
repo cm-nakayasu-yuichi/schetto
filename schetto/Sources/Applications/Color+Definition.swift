544da30e60f77d2355eac04168858b79b7438b28
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-11-07 21:22:21 +0900

    ナビゲーションのレイアウト

--- schetto/Sources/Applications/Color+Definition.swift
+++ schetto/Sources/Applications/Color+Definition.swift
@@ -6,6 +6,9 @@ import UIKit
 
 extension UIColor {
     
+    static let baseNavigationBackground = #colorLiteral(red: 0.8039215686, green: 0.8117647059, blue: 0.8274509804, alpha: 1)
+    static let baseNavigationForeground = #colorLiteral(red: 0.4017234952, green: 0.4219859471, blue: 0.4625108507, alpha: 1)
+    
     static let primaryButton = #colorLiteral(red: 0.4666666687, green: 0.7327202691, blue: 0.2666666806, alpha: 1)
     static let primaryButtonHighlighted = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
     static let clearHighlighted = #colorLiteral(red: 0.7498688102, green: 0.7498688102, blue: 0.7498688102, alpha: 0.5)
