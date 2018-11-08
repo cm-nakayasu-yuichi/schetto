3ee4836ac8111029fffa879782c1a52abf2a776c
中安佑一 <yuichi.nakayasu@gmail.com>
2018-11-09 00:14:43 +0900

    キーボードマネージャのイニシャライザ改修

--- schetto/Sources/Utils/KeyboardManager.swift
+++ schetto/Sources/Utils/KeyboardManager.swift
@@ -43,7 +43,8 @@ class KeyboardManager {
         return distance
     }
     
-    init() {
+    init(delegate: KeyboardManagerDelegate?) {
+        self.delegate = delegate
         observeKeyboardEvents()
     }
     
