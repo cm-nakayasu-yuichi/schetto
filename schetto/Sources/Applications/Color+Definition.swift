75ebafc63d61ffb18771f522abd13a0ece9f47ec
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2018-11-08 11:48:34 +0900

    ClearButtonの仕様変更

--- schetto/Sources/Applications/Color+Definition.swift
+++ schetto/Sources/Applications/Color+Definition.swift
@@ -6,6 +6,8 @@ import UIKit
 
 extension UIColor {
     
+    static let clearButtonDefaultHighlighted = #colorLiteral(red: 0.7498688102, green: 0.7498688102, blue: 0.7498688102, alpha: 0.5)
+    
     static let baseNavigationBackground = #colorLiteral(red: 0.8039215686, green: 0.8117647059, blue: 0.8274509804, alpha: 1)
     static let baseNavigationForeground = #colorLiteral(red: 0.4017234952, green: 0.4219859471, blue: 0.4625108507, alpha: 1)
     
